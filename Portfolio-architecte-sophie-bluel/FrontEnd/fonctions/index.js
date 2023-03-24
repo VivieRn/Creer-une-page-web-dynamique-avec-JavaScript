5afbdb6a30e7865df8d51f7d8d1e9225bb2ddf78
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-03-24 11:47:53 +0100

    Ajout gestion du display btn login et logout

--- Portfolio-architecte-sophie-bluel/FrontEnd/fonctions/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/fonctions/index.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
   if (isAdmin) {
     const btnAdmin = document.getElementById("btnAdmin");
     btnAdmin.style.display = "block";
-    console.log("Admin !!");
+    const btnLogout = document.getElementById("logout");
+    btnLogout.style.display = "block";
+    const btnLogin = document.getElementById("login");
+    btnLogin.style.display = "none";
   }
 });
+
+function logout() {
+  const btnLogout = document.getElementById("logout");
+  console.log(btnLogout);
+  btnLogout.addEventListener("click", function () {
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("isAdmin");
+    window.location.href = "login.html";
+  });
+}
