5e516b20e08634c8361fde1e5ca7d23487ef7fd7
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-04-25 10:48:39 +0200

    Révision de la gestion des erreurs

--- Portfolio-architecte-sophie-bluel/FrontEnd/fonctions/fetchDeleteImage.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/fonctions/fetchDeleteImage.js
@@ -1,24 +1,19 @@
 import { getTokenFromCache } from "./getTokenFromCache.js";
 
 export const fetchDeleteImage = async (imageId) => {
-  try {
-    const token = await getTokenFromCache();
-    const response = await fetch(`http://localhost:5678/api/works/${imageId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const responseData = await response.json();
-    console.log(response);
-    if (!response.ok) {
-      throw new Error(responseData.message || "Unable to delete image.");
-    }
-    const cardImage = document.querySelector(`[data-id="${imageId}"]`);
-    if (cardImage) {
-      cardImage.remove();
-    }
-  } catch (err) {
-    return;
+  const token = await getTokenFromCache();
+  const response = await fetch(`http://localhost:5678/api/works/${imageId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const responseData = await response.json();
+  if (!response.ok) {
+    alert("Echec de la suppression, merci de vérifier votre connexion.");
+  }
+  const cardImage = document.querySelector(`[data-id="${imageId}"]`);
+  if (cardImage) {
+    cardImage.remove();
   }
 };
--- Portfolio-architecte-sophie-bluel/FrontEnd/fonctions/handlePictureSubmit.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/fonctions/handlePictureSubmit.js
@@ -14,54 +14,46 @@ export const handlePictureSubmit = async function (e) {
 
   formData.set("category", category);
 
-  for (let item of formData) {
-    console.log(item[0], item[1]);
-  }
+  const token = await getTokenFromCache();
+  const response = await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      accept: "application/json",
+    },
+    body: formData,
+    mode: "cors",
+  });
 
-  try {
-    const token = await getTokenFromCache();
-    const response = await fetch("http://localhost:5678/api/works", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        accept: "application/json",
-      },
-      body: formData,
-      mode: "cors",
-    });
+  if (!response.ok) {
+    alert("Utilisateur non autorisé, merci de vous reconnecter.");
+  } else {
+    const responseData = await response.json();
 
-    if (!response.ok) {
-      return;
-    } else {
-      const responseData = await response.json();
+    // Création de la nouvelle carte image
+    const newCardImage = {
+      id: responseData.id,
+      title: formData.get("title"),
+      imageUrl: responseData.imageUrl,
+      categoryId: categories[formData.get("category")],
+    };
 
-      // Création de la nouvelle carte image
-      const newCardImage = {
-        id: responseData.id,
-        title: formData.get("title"),
-        imageUrl: responseData.imageUrl,
-        categoryId: categories[formData.get("category")],
-      };
+    // Création des éléments HTML de la carte image
+    const pieceElement = document.createElement("figure");
+    const imageElement = document.createElement("img");
+    imageElement.src = newCardImage.imageUrl;
+    const nomElement = document.createElement("figcaption");
+    nomElement.innerText = newCardImage.title;
 
-      // Création des éléments HTML de la carte image
-      const pieceElement = document.createElement("figure");
-      const imageElement = document.createElement("img");
-      imageElement.src = newCardImage.imageUrl;
-      const nomElement = document.createElement("figcaption");
-      nomElement.innerText = newCardImage.title;
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
 
-      pieceElement.appendChild(imageElement);
-      pieceElement.appendChild(nomElement);
+    // Ajout de la nouvelle carte image à la galerie
+    const gallery = document.querySelector("#gallery");
+    gallery.appendChild(pieceElement);
 
-      // Ajout de la nouvelle carte image à la galerie
-      const gallery = document.querySelector("#gallery");
-      gallery.appendChild(pieceElement);
-
-      // Remplacement du contenu du modal avec la nouvelle carte image
-      replaceModalContent(newCardImage);
-    }
-  } catch (error) {
-    return;
+    // Remplacement du contenu du modal avec la nouvelle carte image
+    replaceModalContent(newCardImage);
   }
 };
 
